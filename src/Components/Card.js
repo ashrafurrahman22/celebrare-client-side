9e7b37a04a538059a38bce7b281dea086f8fb00d
ASHRAFUR RAHMAN <you@example.com>
2022-10-13 15:39:11 +0600

    home-page-updated

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import background from '../assets/Background.jpg'
-import card1 from '../assets/Page1.jpg'
-import card2 from '../assets/Page2.jpg'
-import card3 from '../assets/Page3.jpg'
+import React from "react";
+import background from "../assets/Background.jpg";
+import card1 from "../assets/Page1.jpg";
+import card2 from "../assets/Page2.jpg";
+import card3 from "../assets/Page3.jpg";
 
 const Card = () => {
-    return (
-        <div>
-            <img src={background} alt="" />
-            <img src={card1} alt="" />
-            <img src={card2} alt="" />
-            <img src={card3} alt="" />
+  return (
+    <div>
+      <div className="relative">
+        <img className="h-64 w-full rounded-3xl" src={background} alt="" />
+        <div className="absolute top-8">
+            <h2 className=" text-white ml-96 pl-20 text-3xl">Digital Card Details</h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Card from '../../Components/Card';
 import Features from '../../Components/Features';
 
 const Home = () => {
     return (
         <div>
-
+            <Card/>
             <Features/>
 
         </div>
